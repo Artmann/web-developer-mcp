5270ecb58793b3ea2dcd2e61dc2b5a16bbec2ea3
Christoffer Artmann <artgaard@gmail.com>
2025-09-25 00:07:13 +0200

    feat: Add navigation and console tracking.

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ let browserInstance: Browser | null = null
 async function getBrowserInstance(): Promise<Browser> {
   if (!browserInstance) {
     try {
-      console.log('Launching browser...')
+      console.error('Launching browser...')
 
       browserInstance = await chromium.launch({
         headless: false,
-        timeout: 2_000
+        timeout: 5_000
       })
 
-      console.log('Browser launched successfully')
+      console.error('Browser launched successfully')
     } catch (error) {
       console.error('Error launching browser:', error)
 
@@ -26,55 +26,84 @@ async function getBrowserInstance(): Promise<Browser> {
   return browserInstance
 }
 
+let consoleBuffer: string[] = []
+
 async function visitPage(url: string) {
-  console.log(`Visiting page: ${url}`)
+  console.error(`Visiting page: ${url}`)
+
+  consoleBuffer = []
+
   const browser = await getBrowserInstance()
   const context = await browser.newContext()
   const page = await context.newPage()
+
+  page.on('console', (msg) => {
+    consoleBuffer.push(`[${msg.type()}] ${msg.text()}`)
+  })
 
   await page.goto(url)
 }
 
 async function main() {
-  console.log('Starting MCP server...')
-  await visitPage('https://example.com')
+  console.error('Starting MCP server...')
 
-  // const server = new McpServer({
-  //   name: 'demo-server',
-  //   version: '1.0.0'
-  // })
+  const server = new McpServer({
+    name: 'demo-server',
+    version: '1.0.0'
+  })
 
-  // server.registerTool(
-  //   'navigate-to-page',
-  //   {
-  //     title: 'Navigate to Page',
-  //     description: 'Navigate to a specific page',
-  //     inputSchema: { url: z.string() }
-  //   },
-  //   async ({ url }) => {
-  //     await visitPage(url)
+  server.registerTool(
+    'navigate-to-page',
+    {
+      title: 'Navigate to Page',
+      description: 'Navigate to a specific page',
+      inputSchema: { url: z.string() }
+    },
+    async ({ url }) => {
+      await visitPage(url)
 
-  //     return {
-  //       content: [{ type: 'text', text: `Navigated to ${url}.` }]
-  //     }
-  //   }
-  // )
+      return {
+        content: [{ type: 'text', text: `Navigated to ${url}.` }]
+      }
+    }
+  )
 
-  // const transport = new StdioServerTransport()
+  server.registerTool(
+    'read-console-logs',
+    {
+      title: 'Read Console Logs',
+      description: 'Read console logs from the current page'
+    },
+    async () => {
+      return {
+        content: [
+          {
+            type: 'text',
+            text:
+              consoleBuffer && consoleBuffer.length > 0
+                ? consoleBuffer.join('\n')
+                : 'No console logs available.'
+          }
+        ]
+      }
+    }
+  )
 
-  //await server.connect(transport)
+  const transport = new StdioServerTransport()
+
+  await server.connect(transport)
 }
 
-main()
-  .catch((error) => {
-    console.error(error)
+main().catch((error) => {
+  console.error(error)
 
-    process.exit(1)
-  })
-  .finally(async () => {
-    const browser = await getBrowserInstance()
+  process.exit(1)
+})
+// .finally(async () => {
+//   console.error('Shutting down the browser.')
+//   const browser = await getBrowserInstance()
 
-    if (browser) {
-      await browser.close()
-    }
-  })
+//   if (browser) {
+//     await browser.close()
+//   }
+// })
