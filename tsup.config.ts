4dde4a7669248eb653d6a777495ef5583e9a197c
Christoffer Artmann <artgaard@gmail.com>
2025-09-28 16:44:21 +0200

    feat: Build the CLI.

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  target: 'node18',
+  outDir: 'dist',
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  shims: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+})
